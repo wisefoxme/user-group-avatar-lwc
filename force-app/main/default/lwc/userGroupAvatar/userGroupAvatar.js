70ae5860cd7de7efd73c15a4771df763147d1a4e
Renato Oliveira <renato.oliveira@dogeforce.com>
2025-09-15 12:32:32 -0300

    feat: adds group name as the default label
    also adds tests

--- force-app/main/default/lwc/userGroupAvatar/userGroupAvatar.js
+++ force-app/main/default/lwc/userGroupAvatar/userGroupAvatar.js
@@ -1,12 +1,28 @@
 import getGroupMembers from "@salesforce/apex/UserGroupAvatarController.getGroupMembers";
+import { getFieldValue, getRecord } from "lightning/uiRecordApi";
 import { api, LightningElement, wire } from "lwc";
+import GROUP_NAME_FIELD from "@salesforce/schema/Group.Name";
 
 export default class UserGroupAvatar extends LightningElement {
   @api recordId;
   @api fallbackIconName = "standard:user";
   @api maxAvatars = 5;
-  @api label;
+  @api variant;
+  groupName;
+  _label;
   groupMembers = [];
+
+  @api
+  get label() {
+    return this._label || this.groupName || "Group Members";
+  }
+  set label(value) {
+    this._label = value;
+  }
+
+  get showLabel() {
+    return this.variant !== "label-hidden" && !!this.label;
+  }
 
   @wire(getGroupMembers, { groupIdOrName: "$recordId" })
   wiredGroupMembers({ error, data }) {
@@ -14,6 +30,15 @@ export default class UserGroupAvatar extends LightningElement {
       this.groupMembers = data.slice(0, this.maxAvatars);
     } else if (error) {
       console.error("Error fetching group members:", error);
+    }
+  }
+
+  @wire(getRecord, { recordId: "$recordId", fields: [GROUP_NAME_FIELD] })
+  wiredGroupName({ error, data }) {
+    if (data) {
+      this.groupName = getFieldValue(data, GROUP_NAME_FIELD);
+    } else if (error) {
+      console.error("Error fetching group name:", error);
     }
   }
 
